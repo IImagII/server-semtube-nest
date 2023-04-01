343d565d2c4df1f85acafabaa81ecd65e313815f
iimagii <mag_1@ukr.net>
2023-04-01 20:09:25 +0300

    сделал логику добавления коментариев

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
-import { CommentService } from './comment.service';
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CommentService } from './comment.service'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  UsePipes,
+  ValidationPipe,
+  Post
+} from '@nestjs/common'
+import { CommentDto } from './comment.dto'
+import { CurrentUser } from 'src/user/user.decorator'
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  //Созданеи коментария
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth() //только для авторизированных пользователей
+  //@Body() - нужно для получения данных поотм их передает в наш валидатор
+  async createComment(@CurrentUser('id') id: string, @Body() dto: CommentDto) {
+    //@CurrentUser - тут берем конкретного user
+    return this.commentService.create(+id, dto)
+  }
 }
